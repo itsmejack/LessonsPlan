f2e40c28f772eaf00658ff3ecf3ce7ae76d4846e
ItsMeJack <jacekxpiw@gmail.com>
2024-01-15 01:03:07 +0100

    Initial commit

---
+++ src/test/java/com/backend/lessonsPlan/LessonsPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.lessonsPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonsPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
