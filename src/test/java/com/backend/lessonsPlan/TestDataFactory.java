6bc0330569b2fc49a16dad517e621826be087066
itsmejack <22753078+itsmejack@users.noreply.github.com>
2024-03-29 20:04:28 +0100

    Added simple JUnits for all entities.

--- src/test/java/com/backend/lessonsPlan/TestDataFactory.java
+++ src/test/java/com/backend/lessonsPlan/TestDataFactory.java
@@ -1,11 +1,26 @@
 package com.backend.lessonsPlan;
 
 import com.backend.lessonsPlan.Entities.IntervalEntity;
+import com.backend.lessonsPlan.Entities.LessonEntity;
+import com.backend.lessonsPlan.Entities.StudentEntity;
+import com.backend.lessonsPlan.Entities.TeacherEntity;
 import com.backend.lessonsPlan.Model.Day;
 import com.backend.lessonsPlan.Model.NumberOfLesson;
 
 public class TestDataFactory {
     public static IntervalEntity createInterval() {
         return IntervalEntity.builder().id(1L).intervalDay(Day.MON).numberOfLesson(NumberOfLesson.LESSON_1).build();
+    }
+
+    public static LessonEntity createLesson() {
+        return LessonEntity.builder().lessonId(1L).lessonDay(Day.MON).numberOfLesson(NumberOfLesson.LESSON_1).build();
+    }
+
+    public static StudentEntity createStudent() {
+        return StudentEntity.builder().name("name").build();
+    }
+
+    public static TeacherEntity createTeacher() {
+        return TeacherEntity.builder().name("name").build();
     }
 }
--- src/test/java/com/backend/lessonsPlan/database/DatabaseTests.java
+++ src/test/java/com/backend/lessonsPlan/database/DatabaseTests.java
@@ -1,12 +1,19 @@
 package com.backend.lessonsPlan.database;
 
 import com.backend.lessonsPlan.Entities.IntervalEntity;
+import com.backend.lessonsPlan.Entities.LessonEntity;
+import com.backend.lessonsPlan.Entities.StudentEntity;
+import com.backend.lessonsPlan.Entities.TeacherEntity;
 import com.backend.lessonsPlan.Repositories.IntervalRepository;
+import com.backend.lessonsPlan.Repositories.LessonRepository;
+import com.backend.lessonsPlan.Repositories.StudentRepository;
+import com.backend.lessonsPlan.Repositories.TeacherRepository;
 import com.backend.lessonsPlan.TestDataFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -18,18 +25,46 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class DatabaseTests {
 
-    private IntervalRepository intervalRepository;
+    private final IntervalRepository intervalRepository;
+    private final LessonRepository lessonRepository;
+    private final StudentRepository studentRepository;
+    private final TeacherRepository teacherRepository;
 
     @Autowired
-    public DatabaseTests(IntervalRepository intervalRepository) {
+    public DatabaseTests(IntervalRepository intervalRepository, LessonRepository lessonRepository, StudentRepository studentRepository, TeacherRepository teacherRepository) {
         this.intervalRepository = intervalRepository;
+        this.lessonRepository = lessonRepository;
+        this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
     }
     @Test
     public void assertInterval() {
         IntervalEntity intervalEntity = TestDataFactory.createInterval();
-        intervalRepository.save(intervalEntity);
-        Optional<IntervalEntity> result = intervalRepository.findById(intervalEntity.getId());
+        assertInternal(intervalEntity, intervalEntity.getId(), intervalRepository);
+    }
+
+    @Test
+    public void assertLesson() {
+        LessonEntity lessonEntity = TestDataFactory.createLesson();
+        assertInternal(lessonEntity, lessonEntity.getLessonId(), lessonRepository);
+    }
+
+    @Test
+    public void assertStudent() {
+        StudentEntity studentEntity = TestDataFactory.createStudent();
+        assertInternal(studentEntity, studentEntity.getName(), studentRepository);
+    }
+
+    @Test
+    public void assertTeacher() {
+        TeacherEntity teacherEntity = TestDataFactory.createTeacher();
+        assertInternal(teacherEntity, teacherEntity.getName(), teacherRepository);
+    }
+
+    private void assertInternal(Object objectEntity, Object id, CrudRepository repository) {
+        repository.save(objectEntity);
+        Optional<?> result = repository.findById(id);
         assertThat(result).isPresent();
-        assertThat(result.get()).isEqualTo(intervalEntity);
+        assertThat(result.get()).isEqualTo(objectEntity);
     }
 }
